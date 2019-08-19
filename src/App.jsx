c6024fae6a5bb981d018214a23137c523e6a45b2
ngel rojas p <aomarrojasp@gmail.com>
2019-08-19 20:13:57 -0300

    add media query with scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 export const App = () => (
     <h1>hello strech usin jsx</h1>
 )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '../themes/normal/normal.scss'
 import {App} from './App.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'))
